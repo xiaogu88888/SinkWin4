6d41d3725e16216a4bfdba4a2e3f6e7016466377
btccoin8866@gmail.com <xiaogu202011@163.com>
2025-09-29 18:01:42 +0700

    更新内容

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { AreaChart } from 'lucide-vue-next'
 import { GitHubIcon } from 'vue3-simple-icons'
-import heroImg from '@/assets/images/hero.svg?raw'
+import heroImg from '@/assets/images/hero.png?raw'
 
 const { title, description, github } = useAppConfig()
 </script>
--- app/components/layouts/Footer.vue
+++ app/components/layouts/Footer.vue
@@ -14,11 +14,11 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
       >{{ title }}</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://WIN4.COM"
         target="_blank"
-        title="HTML.ZONE"
+        title="WIN4.COM"
       >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
+        &copy; {{ new Date().getFullYear() }} Products of WIN4.COM
       </a>
       <span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
